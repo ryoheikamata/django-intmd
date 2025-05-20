adf22c5617c36145f1146ff27a4e7e525994b1f7
ryohei_kamata <r.k.pitschi@gmail.com>
2025-05-20 22:57:26 +0900

    makefile

--- matching_app/urls.py
+++ matching_app/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.http import HttpResponse  # 正しいインポート文
 from django.urls import path
 
@@ -5,5 +8,8 @@ from django.urls import path
 def home(request):
     return HttpResponse("hello world!!!!!!!!!!!!!!!!!!!!")
 
-
-urlpatterns = [path("home", home, name="home")]
+urlpatterns = (
+    [path("home", home, name="home")]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
