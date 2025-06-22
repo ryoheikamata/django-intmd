45582f4f771ab4ac2cd26e2b3ba488cf1ffa405e
ryohei_kamata <r.k.pitschi@gmail.com>
2025-06-22 11:06:16 +0900

    7-8: 募集削除まで

--- matching_app/urls.py
+++ matching_app/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 from matching_app.views.index import index
 from matching_app.views.login import login_view, logout_view
-from matching_app.views.recruitment import recruitment_timeline, recruitment_detail, recruitment_create, recruitment_update
+from matching_app.views.recruitment import recruitment_timeline, recruitment_detail, recruitment_create, recruitment_update, recruitment_delete
 from matching_app.views.signup import signup
 from matching_app.views.user_profile import user_home, user_profile_update, user_profile_list, user_profile_detail
 from matching_app.views.verify import send_new_verification_code, verify_email
@@ -29,6 +29,7 @@ urlpatterns = (
         path("recruitments/<int:pk>/", recruitment_detail, name="recruitment_detail"),  # 追加
         path("recruitments/create/", recruitment_create, name="recruitment_create"),  # 追加
         path("recruitments/<int:pk>/update/", recruitment_update, name="recruitment_update"),  # 追加
+        path("recruitments/<int:pk>/delete/", recruitment_delete, name="recruitment_delete"),  # 追加
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
