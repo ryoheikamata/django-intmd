4a7b015560211b97582f749fc6c0abb3fe4275cb
ryohei_kamata <r.k.pitschi@gmail.com>
2025-06-22 13:57:14 +0900

    9-2: ASGIサーバーの設定まで

--- django_intmd/asgi.py
+++ django_intmd/asgi.py
@@ -1,7 +1,16 @@
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_intmd.settings.base")
 
-application = get_asgi_application()
+# application = get_asgi_application()
+# Initialize django ASGI application before import websocket_urlpatterns in order to avoid circular import.
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+    }
+)
