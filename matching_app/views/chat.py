1cd00a3c0c41406c0578d097d7ff69b37b11dae3
ryohei_kamata <r.k.pitschi@gmail.com>
2025-06-22 20:11:55 +0900

    9-8: チャットルーム一覧まで

--- matching_app/views/chat.py
+++ matching_app/views/chat.py
@@ -50,3 +50,29 @@ def chat_room_create(request: HttpRequest, user_id: int) -> HttpResponse:
         return HttpResponseServerError()
 
     return redirect("chat_room", room_id=room.id)
+
+@login_required
+@require_http_methods(["GET"])
+def chat_room_list(request: HttpRequest) -> HttpResponse:
+    room_members = RoomMember.objects.filter(user=request.user).select_related("room")
+
+    room_data_dict = {}
+    for room_member in room_members:
+        room = room_member.room
+        try:
+            opposite_user = room.get_opposite_user(current_user=request.user)
+        except NoOppositeUserError as ex:
+            logger.error("no opposite user", room=room, error=ex)
+            return HttpResponseServerError()
+
+        last_message = room.messages.order_by("-created_at").first()
+        room_data_dict[room] = {
+            "opposite_user": opposite_user,
+            "last_message": last_message,
+        }
+
+    return render(
+        request,
+        "chat_room_list.html",
+        {"room_data_dict": room_data_dict, "user": request.user},
+    )
